c8cf5aae7f5efa7284ff8e3d7807f37603de6d22
Maciek <ordos18@gmail.com>
2020-03-13 12:59:26 +0100

    chapter 2

---
+++ Demo/ARM7_LPC2129_Keil_RVDS/led.h
@@ -0,0 +1,6 @@
+
+void LedInit (void);
+void LedStepLeft (void);
+void LedStepRight (void);
+void LedOn (unsigned char ucLedIndeks);
+void Led_Toggle(unsigned char ucLedIndeks);
---
+++ Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -0,0 +1,35 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "led.h"
+
+void Delay(unsigned int uiMiliSec) {
+	
+	unsigned int uiLoopCtr, uiDelayLoopCount;
+	uiDelayLoopCount = uiMiliSec*12000;
+	for(uiLoopCtr=0;uiLoopCtr<uiDelayLoopCount;uiLoopCtr++) {}
+}
+
+void Led0Blink( void *pvParameters ){
+
+	while(1){
+		Led_Toggle(0);
+		Delay(500);
+	}
+}
+
+void Led1Blink( void *pvParameters ){
+
+	while(1){
+		Led_Toggle(1);
+		Delay(500);
+	}
+}
+
+int main(void){
+	
+	LedInit();
+	xTaskCreate(Led0Blink, NULL , 100 , NULL, 2 , NULL );
+	xTaskCreate(Led1Blink, NULL , 100 , NULL, 2 , NULL );
+	vTaskStartScheduler();
+	while(1);
+}
