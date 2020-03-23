49e43cfec6c2f8c14f373dcd6a2c2681f88e5568
Maciek <ordos18@gmail.com>
2020-03-23 11:14:52 +0100

    fixed time measure

--- Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -6,56 +6,52 @@
 #include "uart.h"
 #include "keyboard.h"
 
+	
+xSemaphoreHandle xSemaphore;
+
+
 void Rtos_Transmitter_SendString (char pcString[]) {
 	
+	TickType_t tTickStart;
 	char cStringToSend[TRANSMITER_SIZE];
 	
-	CopyString(pcString, cStringToSend);
-	ReplaceCharactersInString(cStringToSend, '\n', ':');
-	AppendUIntToString(xTaskGetTickCount(), cStringToSend);
-	AppendString("\n",cStringToSend);
-	Transmitter_SendString(cStringToSend);
+	tTickStart = xTaskGetTickCount();
+	if (pdTRUE == xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
+		CopyString(pcString, cStringToSend);
+		AppendString(":",cStringToSend);
+		AppendUIntToString(xTaskGetTickCount()-tTickStart, cStringToSend);
+		AppendString("\n",cStringToSend);
+		Transmitter_SendString(cStringToSend);
+		while (eTransmitter_GetStatus()!=FREE){};
+		xSemaphoreGive(xSemaphore);
+	}
 }
 
 void LettersTx (void *pvParameters){
 	
-	xSemaphoreHandle xSemaphore = *((xSemaphoreHandle *) pvParameters);
-	
 	while(1){
-		if (pdTRUE == xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
-			Rtos_Transmitter_SendString("-ABCDEEFGH-\n");
-			while (eTransmitter_GetStatus()!=FREE){};
-			xSemaphoreGive(xSemaphore);
-			vTaskDelay(300);
-		}
+		Rtos_Transmitter_SendString("-ABCDEEFGH-");
+		vTaskDelay(300);
 	}
 }
 
 void KeyboardTx (void *pvParameters){
 	
-	xSemaphoreHandle xSemaphore = *((xSemaphoreHandle *) pvParameters);
-	
 	while(1){
 		if( RELEASED != eKeyboardRead() ){
-			if (pdTRUE == xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
-				Transmitter_SendString("-Keyboard-\n");
-				while (eTransmitter_GetStatus()!=FREE){};
-				xSemaphoreGive(xSemaphore);
-				vTaskDelay(300);
-			}
+			Rtos_Transmitter_SendString("-Keyboard-");
+			vTaskDelay(300);
 		}
 	}
 }
 
 int main( void ){
 	
-	xSemaphoreHandle xSemaphore;
-	
 	vSemaphoreCreateBinary(xSemaphore);
 	KeyboardInit();
 	UART_InitWithInt(300);
-	xTaskCreate(LettersTx, NULL, 128, &xSemaphore, 1, NULL );
-	xTaskCreate(KeyboardTx, NULL, 128, &xSemaphore, 1, NULL );
+	xTaskCreate(LettersTx, NULL, 128, NULL, 1, NULL );
+	xTaskCreate(KeyboardTx, NULL, 128, NULL, 1, NULL );
 	vTaskStartScheduler();
 	while(1);
 }
