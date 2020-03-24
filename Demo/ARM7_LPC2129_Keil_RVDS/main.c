e8b46794e778a4208ec00c54178b5e203973f108
Maciek <ordos18@gmail.com>
2020-03-24 11:50:41 +0100

    chapter 5.2 ex. 4

--- Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -2,6 +2,7 @@
 #include "task.h"
 #include "led.h"
 #include "uart.h"
+#include "string.h"
 
 void UartRx( void *pvParameters ) {
 	
@@ -9,7 +10,11 @@ void UartRx( void *pvParameters ) {
 	
 	while(1){
 		UART_GetString(acBuffer);
-		Led_Toggle(acBuffer[0]-'0');
+		if (EQUAL == eCompareString(acBuffer, "zero")) {
+			Led_Toggle(0);
+		} else if (EQUAL == eCompareString(acBuffer, "jeden")) {
+			Led_Toggle(1);
+		}
 	}
 }
 
